3002ed7f4f69882269b91eb625d0d5996867c22d
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2023-08-30 23:20:35 +0200

    .

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,17 +1,17 @@
-// Modal Setup
-var modal = document.getElementById('modal');
 
-var modalClose = document.getElementById('modal-close');
+var modal = document.querySelector('#modal');
+
+var modalClose = document.querySelector('#modal-close');
 modalClose.addEventListener('click', function () {
 	modal.style.display = 'none';
 });
 
-// global handler
+
 document.addEventListener('click', function (e) {
 	if (e.target.className.indexOf('modal-target') !== -1) {
 		var img = e.target;
-		var modalImg = document.getElementById('modal-content');
-		var captionText = document.getElementById('modal-caption');
+		var modalImg = document.querySelector('#modal-content');
+		var captionText = document.querySelector('#modal-caption');
 		modal.style.display = 'block';
 		modalImg.src = img.src;
 		captionText.innerHTML = img.alt;
